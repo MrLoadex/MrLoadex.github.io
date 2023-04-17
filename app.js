1e3c8c087339008a90037e656728a039d502b140
Julian daniel Nuñez Tellechea <101680130+MrLoadex@users.noreply.github.com>
2023-04-17 20:56:17 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,110 +1,58 @@
-const fecha = document.querySelector('#fecha');
-const lista = document.querySelector('#lista');
-const input = document.querySelector('#input');
-const btnAgregar = document.querySelector('#enter');
-const check = 'fa-check-circle';
-const onCheck = 'fa-circle';
-const LineThrough = 'line-throuhg';
-let id;
+// script.js
+const input = document.getElementById('input');
+const btnAgregar = document.getElementById('btnAgregar');
+const lista = document.getElementById('lista');
+
 let LIST = [];
 
-//Fecha de la TL
-const FECHA = new Date();
-fecha.innerHTML = FECHA.toLocaleDateString('es-MX',{weekday:'long', month:'short', day:'numeric'});
-
-function agregarTarea(tarea,id,realizado,eliminado){
-    // Si eliminado existe no ejecuta nada
-    if(eliminado){
-        return;
-    }
-
-    const REALIZADO = realizado ? check : onCheck;
-    const LINE = realizado ? LineThrough : '';
-
-    const elemento = `
-        <li id="elemento">
-            <i class="far ${REALIZADO}" data="realizado" id="${id}"></i>
-            <p class="text ${LINE}">${tarea}</p>
-            <i class="fas fa-trash de" data="eliminado" id="${id}"></i> 
-        </li>
-    `;
-
-    // Este indica donde los va insertar segun los parametros
-    lista.insertAdjacentHTML("beforeend", elemento);
-}
-
-// Funcion tarea realizada
-function tareaRealizada(element){
-    //Si detecta que esta en uno lo cambia por el otro y asi
-    element.classList.toggle(check);
-    element.classList.toggle(onCheck);
-    element.parentNode.querySelector('.text').classList.toggle(LineThrough);
-    LIST[element.id].realizado = LIST[element.id].realizado ? false : true;
-}
-
-//Tarea eliminada
-function TareaEliminado(element){
-    element.eliminado = true;
-    element.parentNode.parentNode.removeChild(element.parentNode);
-    LIST[element.id].eliminado = true;
-    localStorage.setItem('TODO',JSON.stringify(LIST));
-}
-
-//Evento con el botón de agregar
+// Evento al hacer clic en el botón de "Agregar"
 btnAgregar.addEventListener('click', () => {
     const tarea = input.value;
-    if(tarea){
-        agregarTarea(tarea,id,false,false);
-        LIST.push({
-            nombre: tarea,
-            id: id,
-            realizado:false,
-            eliminado: false
-        });
-    };
-
-    localStorage.setItem('TODO',JSON.stringify(LIST));
-
-    input.value = "";
-    id++;
+    if (tarea) {
+        agregarTarea(tarea);
+        LIST.push(tarea);
+        localStorage.setItem('TODO', JSON.stringify(LIST));
+    }
+    input.value = '';
 });
 
-//Este responde con el enter pero hace lo mismo que la de arriba
-document.addEventListener('keyup', function(event){
-    if(event.key=='Enter'){
+// Evento al presionar la tecla "Enter" en el input de texto
+input.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
         const tarea = input.value;
-        if(tarea){
-            agregarTarea(tarea,id,false,false);
-            LIST.push({
-                nombre: tarea,
-                id: id,
-                realizado:false,
-                eliminado: false
-            });
-        };
-
-        localStorage.setItem('TODO',JSON.stringify(LIST));
-
-        input.value = "";
-        id++;
+        if (tarea) {
+            agregarTarea(tarea);
+            LIST.push(tarea);
+            localStorage.setItem('TODO', JSON.stringify(LIST));
+        }
+        input.value = '';
     }
 });
 
-lista.addEventListener('click',function(event){
+// Función para agregar una tarea a la lista
+function agregarTarea(tarea) {
+    const elemento = `
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <span>${tarea}</span>
+            <div>
+                <i class="fas fa-check-circle mr-2"></i>
+                <i class="fas fa-trash"></i>
+            </div>
+        </li>
+    `;
+    lista.insertAdjacentHTML('beforeend', elemento);
+}
+
+// Evento al hacer clic en el ícono de "Realizado" o "Eliminar"
+lista.addEventListener('click', (event) => {
     const element = event.target;
-    const elementData = element.attributes.data.value;
-
-    if(elementData === 'realizado'){
-        tareaRealizada(element);
-    }else if(elementData === 'eliminado'){
-        TareaEliminado(element);
-    };
-    localStorage.setItem('TODO',JSON.stringify(LIST));
-
+    if (element.classList.contains('fa-check-circle')) {
+        element.classList.toggle('text-success');
+        element.parentNode.parentNode.querySelector('span').classList.toggle('text-muted');
+    } else if (element.classList.contains('fa-trash')) {
+        element.parentNode.parentNode.remove();
+        const tarea = element.parentNode.parentNode.querySelector('span').innerText;
+        LIST = LIST.filter((item) => item !== tarea);
+        localStorage.setItem('TODO', JSON.stringify(LIST));
+    }
 });
-
-// Función para limpiar el DOM de elementos de la lista vieja
-function limpiarListaVieja() {
-    const lista = document.getElementById("lista"); // Reemplaza "lista" con el ID de tu lista
-    while (lista.firstChild) {
-        lista
