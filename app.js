c6a32921d273366ca3e2f38c9abb607ca4240ced
Julian daniel Nuñez Tellechea <101680130+MrLoadex@users.noreply.github.com>
2023-04-17 21:25:24 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,53 +6,45 @@ document.getElementById('todo-input').addEventListener('keyup', function(event) 
     }
 });
 
-document.getElementById('doing-button').addEventListener('click', addTask);
-document.getElementById('doing-input').addEventListener('keyup', function(event) {
-    if (event.keyCode === 13) {
-        addTask();
-    }
+document.getElementById('todo-list').addEventListener('click', function(event) {
+    moveTask(event, 'doing-list');
 });
 
-document.getElementById('done-button').addEventListener('click', addTask);
-document.getElementById('done-input').addEventListener('keyup', function(event) {
-    if (event.keyCode === 13) {
-        addTask();
-    }
+document.getElementById('doing-list').addEventListener('click', function(event) {
+    moveTask(event, 'done-list');
+});
+
+document.getElementById('done-list').addEventListener('click', function(event) {
+    moveTask(event, 'todo-list');
 });
 
 function addTask() {
-    const input = event.target.previousElementSibling;
+    const input = document.getElementById('todo-input');
     const task = input.value.trim();
     if (task !== '') {
         const listItem = document.createElement('li');
         listItem.textContent = task;
-        listItem.addEventListener('click', moveTask);
-        input.previousElementSibling.appendChild(listItem);
+        document.getElementById('todo-list').appendChild(listItem);
         input.value = '';
     }
 }
 
-function moveTask() {
-    const listId = event.target.parentNode.parentNode.id;
+function moveTask(event, targetListId) {
     const task = event.target.textContent;
     event.target.parentNode.removeChild(event.target);
 
-    switch (listId) {
-        case 'todo-list':
-            document.getElementById('doing-list').appendChild(createListItem(task));
-            break;
-        case 'doing-list':
-            document.getElementById('done-list').appendChild(createListItem(task));
-            break;
-        case 'done-list':
-            document.getElementById('todo-list').appendChild(createListItem(task));
-            break;
-    }
-}
-
-function createListItem(task) {
     const listItem = document.createElement('li');
     listItem.textContent = task;
-    listItem.addEventListener('click', moveTask);
-    return listItem;
+
+    switch (targetListId) {
+        case 'doing-list':
+            document.getElementById('doing-list').appendChild(listItem);
+            break;
+        case 'done-list':
+            document.getElementById('done-list').appendChild(listItem);
+            break;
+        case 'todo-list':
+            document.getElementById('todo-list').appendChild(listItem);
+            break;
+    }
 }
