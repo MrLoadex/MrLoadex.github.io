7acd8f928680b39783ef784d25ea9a952e282003
Julian daniel Nuñez Tellechea <101680130+MrLoadex@users.noreply.github.com>
2023-04-17 21:10:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // script.js
 const input = document.querySelector('#input');
 const btnAgregar = document.querySelector('#btnAgregar');
-const lista = document.querySelector('#lista');
+const listaPendientes = document.querySelector('#listaPendientes');
+const listaEnProgreso = document.querySelector('#listaEnProgreso');
+const listaRealizadas = document.querySelector('#listaRealizadas');
 
 // Cargar tareas almacenadas en el localStorage
 cargarTareas();
@@ -14,7 +16,7 @@ input.addEventListener('keyup', function(event) {
     }
 });
 
-lista.addEventListener('click', function(event) {
+listaPendientes.addEventListener('click', function(event) {
     const element = event.target;
     const elementData = element.attributes.data.value;
 
@@ -25,56 +27,32 @@ lista.addEventListener('click', function(event) {
     }
 });
 
+listaEnProgreso.addEventListener('click', function(event) {
+    const element = event.target;
+    const elementData = element.attributes.data.value;
+
+    if (elementData === 'pendiente') {
+        tareaPendiente(element);
+    } else if (elementData === 'realizado') {
+        tareaRealizada(element);
+    } else if (elementData === 'eliminado') {
+        tareaEliminada(element);
+    }
+});
+
+listaRealizadas.addEventListener('click', function(event) {
+    const element = event.target;
+    const elementData = element.attributes.data.value;
+
+    if (elementData === 'pendiente') {
+        tareaPendiente(element);
+    } else if (elementData === 'eliminado') {
+        tareaEliminada(element);
+    }
+});
+
 function agregarTarea() {
     const tarea = input.value;
     if (tarea) {
         const tareaElemento = document.createElement('li');
-        tareaElemento.innerHTML = `<i class="far fa-check-circle" data="realizado"></i>
-        <span class="text">${tarea}</span>
-        <i class="fas fa-trash-alt" data="eliminado"></i>`;
-        lista.appendChild(tareaElemento);
-        guardarTarea(tarea);
-        input.value = '';
-    }
-}
-
-function tareaRealizada(element) {
-    element.parentNode.querySelector('.text').classList.toggle('line-through');
-}
-
-function tareaEliminada(element) {
-    element.parentNode.remove();
-    eliminarTarea(element.parentNode.querySelector('.text').textContent);
-}
-
-function guardarTarea(tarea) {
-    const tareas = obtenerTareas();
-    tareas.push(tarea);
-    localStorage.setItem('tareas', JSON.stringify(tareas));
-}
-
-function obtenerTareas() {
-    let tareas = localStorage.getItem('tareas');
-    if (tareas) {
-        return JSON.parse(tareas);
-    } else {
-        return [];
-    }
-}
-
-function eliminarTarea(tarea) {
-    const tareas = obtenerTareas();
-    const index = tareas.indexOf(tarea);
-    if (index !== -1) {
-        tareas.splice(index, 1);
-        localStorage.setItem('tareas', JSON.stringify(tareas));
-    }
-}
-
-function cargarTareas() {
-    const tareas = obtenerTareas();
-    tareas.forEach(tarea => {
-        const tareaElemento = document.createElement('li');
-        tareaElemento.innerHTML = `<i class="far fa-check-circle" data="realizado"></i>
-        <span class="text">${tarea}</span>
-       
+        tareaElemento.innerHTML = `<i class="far fa-check-circle" data="realizado"></i
