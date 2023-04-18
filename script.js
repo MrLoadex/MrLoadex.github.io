1aa764881a6ee3d28c7f93abb98675be2cc2834a
Julian daniel Nuñez Tellechea <101680130+MrLoadex@users.noreply.github.com>
2023-04-17 22:46:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 // script.js
 
 document.addEventListener('DOMContentLoaded', function() {
-    const toDoList = document.getElementById('to-do-list');
-    const doingList = document.getElementById('doing-list');
-    const doneList = document.getElementById('done-list');
-
     const toDoUl = document.getElementById('to-do-ul');
     const doingUl = document.getElementById('doing-ul');
     const doneUl = document.getElementById('done-ul');
@@ -43,51 +39,58 @@ document.addEventListener('DOMContentLoaded', function() {
         localStorage.setItem('tasks', JSON.stringify(tasks));
     }
 
-    // Agregar tarea a la lista de To-Do
+    // Añadir una nueva tarea
     document.getElementById('add-task-form').addEventListener('submit', function(event) {
         event.preventDefault();
         const taskInput = document.getElementById('task-input');
         const taskText = taskInput.value.trim();
         if (taskText !== '') {
             const taskId = 'task-' + Date.now();
-            tasks.push({
-                id: taskId,
-                text: taskText,
-                status: 'to-do'
-            });
             const taskLi = createTaskLi(taskId, taskText);
             toDoUl.appendChild(taskLi);
+            tasks.push({ id: taskId, text: taskText, status: 'to-do' });
+            saveTasks();
             taskInput.value = '';
+        }
+    });
+
+    // Cambiar el estado de una tarea
+    function changeTaskStatus(taskId, newStatus) {
+        const task = tasks.find(task => task.id === taskId);
+        if (task) {
+            task.status = newStatus;
             saveTasks();
         }
-    });
+    }
 
-    // Mover tarea a la lista de Doing o Done
-    document.addEventListener('click', function(event) {
-        const taskLi = event.target.closest('li');
-        if (taskLi && taskLi.parentElement.classList.contains('list-ul')) {
-            const taskId = taskLi.id;
-            const task = tasks.find(t => t.id === taskId);
-            if (task) {
-                if (event.target.parentElement.id === 'to-do-ul') {
-                    task.status = 'doing';
-                    doingUl.appendChild(taskLi);
-                } else if (event.target.parentElement.id === 'doing-ul') {
-                    task.status = 'done';
-                    doneUl.appendChild(taskLi);
-                } else if (event.target.parentElement.id === 'done-ul') {
-                    tasks = tasks.filter(t => t.id !== taskId);
-                    taskLi.remove();
-                }
-                saveTasks();
-            }
+    // Drag and drop
+    new Sortable(toDoUl, {
+        group: 'tasks',
+        animation: 150,
+        onEnd: function(event) {
+            const taskId = event.item.id;
+            changeTaskStatus(taskId, 'to-do');
         }
     });
 
-    // Hacer que los elementos de la lista sean arrastrables
-    Sortable.create(toDoUl, {
-        group: "tasks",
+    new Sortable(doingUl, {
+        group: 'tasks',
         animation: 150,
-        onEnd: function(evt) {
-            const taskId = evt.item.id;
-            const task = tasks.find(t
+        onEnd: function(event) {
+            const taskId = event.item.id;
+            changeTaskStatus(taskId, 'doing');
+        }
+    });
+
+    new Sortable(doneUl, {
+        group: 'tasks',
+        animation: 150,
+        onEnd: function(event) {
+            const taskId = event.item.id;
+            changeTaskStatus(taskId, 'done');
+        }
+    });
+
+    // Cargar tareas guardadas al cargar la página
+    loadSavedTasks();
+});
