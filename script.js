8d499be5ad6f345c025905fd4e98021740cf20a0
Julian daniel Nuñez Tellechea <101680130+MrLoadex@users.noreply.github.com>
2023-04-17 21:52:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,36 @@
-/* style.css */
-body {
-  font-family: Arial, sans-serif;
-}
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const toDoList = document.getElementById('to-do-list');
+    const doingList = document.getElementById('doing-list');
+    const doneList = document.getElementById('done-list');
 
-.container {
-  display: flex;
-  justify-content: space-around;
-  margin-top: 30px;
-}
+    const toDoUl = document.getElementById('to-do-ul');
+    const doingUl = document.getElementById('doing-ul');
+    const doneUl = document.getElementById('done-ul');
 
-.list {
-  flex: 1;
-  margin: 0 10px;
-}
+    let tasks = [];
 
-.list h2 {
-  margin-top: 0;
-}
+    // Agregar tarea a la lista de To-Do
+    document.getElementById('add-task-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        const taskInput = document.getElementById('task-input');
+        const taskText = taskInput.value.trim();
+        if (taskText !== '') {
+            const taskId = 'task-' + Date.now();
+            tasks.push({
+                id: taskId,
+                text: taskText,
+                status: 'to-do'
+            });
+            const taskLi = createTaskLi(taskId, taskText);
+            toDoUl.appendChild(taskLi);
+            taskInput.value = '';
+        }
+    });
 
-.list-ul {
-  list-style-type: none;
-  padding: 0;
-  background-color: #f7f7f7;
-  border-radius: 5px;
-  min-height: 200px;
-  padding: 10px;
-}
-
-.list-ul li {
-  background-color: #fff;
-  padding: 8px;
-  border-radius: 5px;
-  margin-bottom: 5px;
-  cursor: pointer;
-}
-
-.list-ul li:hover {
-  background-color: #f7f7f7;
-}
+    // Mover tarea a la lista de Doing o Done
+    document.addEventListener('click', function(event) {
+        const taskLi = event.target.closest('li');
+        if (taskLi && taskLi.parentElement.classList.contains('list-ul')) {
+            const taskId = taskLi.id;
+            const task = tasks
