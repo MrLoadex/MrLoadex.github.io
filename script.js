956101396e4fcf9293d7b06337171b35ff34b855
Julian daniel Nuñez Tellechea <101680130+MrLoadex@users.noreply.github.com>
2023-04-17 21:51:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,40 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function() {
-    const doItList = document.getElementById('do-it-list');
-    const doingList = document.getElementById('doing-list');
-    const doneList = document.getElementById('done-list');
+/* style.css */
+body {
+  font-family: Arial, sans-serif;
+}
 
-    const doItUl = document.getElementById('do-it-ul');
-    const doingUl = document.getElementById('doing-ul');
-    const doneUl = document.getElementById('done-ul');
+.container {
+  display: flex;
+  justify-content: space-around;
+  margin-top: 30px;
+}
 
-    let tasks = [];
+.list {
+  flex: 1;
+  margin: 0 10px;
+}
 
-    // Agregar tarea a la lista de Do-It
-    document.getElementById('add-task-form').addEventListener('submit', function(event) {
-        event.preventDefault();
-        const taskInput = document.getElementById('task-input');
-        const taskText = taskInput.value.trim();
-        if (taskText !== '') {
-            const taskId = 'task-' + Date.now();
-            tasks.push({
-                id: taskId,
-                text: taskText,
-                status: 'do-it'
-            });
-            const taskLi = createTaskLi(taskId, taskText);
-            doItUl.appendChild(taskLi);
-            taskInput.value = '';
-        }
-    });
+.list h2 {
+  margin-top: 0;
+}
 
-    // Mover tarea a la lista de Doing o Done
-    document.addEventListener('click', function(event) {
-        const taskLi = event.target.closest('li');
-        if (taskLi && taskLi.parentElement.classList.contains('list-ul')) {
-            const taskId = taskLi.id;
-            const task = tasks.find(t => t.id === taskId);
-            if (task) {
-                if (task.status === 'do-it') {
-                    task.status = 'doing';
-                    doItUl.removeChild(taskLi);
-                    doingUl.appendChild(taskLi);
-                } else if (task.status === 'doing') {
-                    task.status = 'done';
-                    doingUl.removeChild(taskLi);
-                    doneUl.appendChild(taskLi);
-                } else if (task.status === 'done') {
-                    const taskIndex = tasks.findIndex(t => t.id === taskId);
-                    tasks.splice(taskIndex, 1);
-                    doneUl.removeChild(taskLi);
-                }
-            }
-        }
-    });
+.list-ul {
+  list-style-type: none;
+  padding: 0;
+  background-color: #f7f7f7;
+  border-radius: 5px;
+  min-height: 200px;
+  padding: 10px;
+}
 
-    // Crear elemento de lista de tareas (li)
-    function createTaskLi(taskId, taskText) {
-        const taskLi = document.createElement('li');
-        taskLi.id = taskId;
-        taskLi.textContent = taskText;
-        return taskLi;
-    }
-});
+.list-ul li {
+  background-color: #fff;
+  padding: 8px;
+  border-radius: 5px;
+  margin-bottom: 5px;
+  cursor: pointer;
+}
+
+.list-ul li:hover {
+  background-color: #f7f7f7;
+}
