1fd534b95c471e6cdf0171642092e05beaf378b2
Julian daniel Nuñez Tellechea <101680130+MrLoadex@users.noreply.github.com>
2023-04-17 21:53:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // script.js
+
 document.addEventListener('DOMContentLoaded', function() {
     const toDoList = document.getElementById('to-do-list');
     const doingList = document.getElementById('doing-list');
@@ -9,6 +10,15 @@ document.addEventListener('DOMContentLoaded', function() {
     const doneUl = document.getElementById('done-ul');
 
     let tasks = [];
+
+    // Función para crear un elemento de lista de tareas
+    function createTaskLi(taskId, taskText) {
+        const taskLi = document.createElement('li');
+        taskLi.id = taskId;
+        taskLi.textContent = taskText;
+        taskLi.classList.add('task');
+        return taskLi;
+    }
 
     // Agregar tarea a la lista de To-Do
     document.getElementById('add-task-form').addEventListener('submit', function(event) {
@@ -33,4 +43,19 @@ document.addEventListener('DOMContentLoaded', function() {
         const taskLi = event.target.closest('li');
         if (taskLi && taskLi.parentElement.classList.contains('list-ul')) {
             const taskId = taskLi.id;
-            const task = tasks
+            const task = tasks.find(t => t.id === taskId);
+            if (task) {
+                if (event.target.parentElement.id === 'to-do-ul') {
+                    task.status = 'doing';
+                    doingUl.appendChild(taskLi);
+                } else if (event.target.parentElement.id === 'doing-ul') {
+                    task.status = 'done';
+                    doneUl.appendChild(taskLi);
+                } else if (event.target.parentElement.id === 'done-ul') {
+                    tasks = tasks.filter(t => t.id !== taskId);
+                    taskLi.remove();
+                }
+            }
+        }
+    });
+});
