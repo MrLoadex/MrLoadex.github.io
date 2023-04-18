0ddfc684c630609411edd1f8d3c9b6078ae01302
Julian daniel Nuñez Tellechea <101680130+MrLoadex@users.noreply.github.com>
2023-04-17 21:34:28 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,29 @@
-// Código JavaScript (app.js)
-document.getElementById('todo-button').addEventListener('click', addTask);
-document.getElementById('todo-input').addEventListener('keyup', function(event) {
-    if (event.keyCode === 13) {
-        addTask();
-    }
+document.querySelectorAll('.task').forEach(task => {
+    task.addEventListener('dragstart', dragStart);
+    task.addEventListener('dragend', dragEnd);
 });
 
-function addTask() {
-    const input = document.getElementById('todo-input');
-    const taskName = input.value;
-    if (taskName.trim() !== '') {
-        const task = createTaskElement(taskName);
-        document.getElementById('todo-list').appendChild(task);
-        input.value = '';
-    }
-}
-
-function createTaskElement(taskName) {
-    const task = document.createElement('li');
-    task.className = 'tarea';
-    task.draggable = true;
-    task.textContent = taskName;
-    task.addEventListener('dragstart', dragStart);
-    return task;
-}
+document.querySelectorAll('.list').forEach(list => {
+    list.addEventListener('dragover', allowDrop);
+    list.addEventListener('drop', drop);
+});
 
 function dragStart(event) {
     event.dataTransfer.setData('text/plain', event.target.id);
+}
+
+function dragEnd(event) {
+    event.target.style.opacity = '1';
 }
 
 function allowDrop(event) {
     event.preventDefault();
 }
 
-function drop(event, targetListId) {
+function drop(event) {
     event.preventDefault();
     const taskId = event.dataTransfer.getData('text/plain');
+    const targetList = event.currentTarget;
     const task = document.getElementById(taskId);
-    const targetList = document.getElementById(targetListId);
     targetList.appendChild(task);
 }
-
