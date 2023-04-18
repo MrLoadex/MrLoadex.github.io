7916d978ba8d94ed597867d8ef2a8aa69247f44f
Julian daniel Nuñez Tellechea <101680130+MrLoadex@users.noreply.github.com>
2023-04-17 21:44:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,85 +1,39 @@
-// Obtener referencias a las listas
-const todoList = document.getElementById('todo-list');
-const doingList = document.getElementById('doing-list');
-const doneList = document.getElementById('done-list');
-
-// Obtener referencias a los elementos de entrada de texto
-const taskInput = document.getElementById('task-input');
-const addTaskBtn = document.getElementById('add-task-btn');
-
-// Agregar evento click al botón de agregar tarea
-addTaskBtn.addEventListener('click', addTask);
-
-// Agregar evento de tecla presionada en el input de tarea
-taskInput.addEventListener('keypress', function(event) {
-    if (event.keyCode === 13) {
-        addTask();
-    }
-});
-
-// Función para agregar una tarea a la lista de To-Do
-function addTask() {
-    const taskText = taskInput.value;
-    if (taskText.trim() === '') {
-        return;
-    }
-
-    const taskElement = createTaskElement(taskText);
-    todoList.appendChild(taskElement);
-
-    taskInput.value = '';
+/* style.css */
+.container {
+    display: flex;
+    justify-content: space-around;
+    margin-top: 50px;
 }
 
-// Función para crear un elemento de tarea
-function createTaskElement(taskText) {
-    const taskElement = document.createElement('div');
-    taskElement.className = 'task';
-    taskElement.textContent = taskText;
-    taskElement.draggable = true;
-    taskElement.addEventListener('dragstart', dragStartHandler);
-    taskElement.addEventListener('dragend', dragEndHandler);
-    return taskElement;
+.list {
+    flex-basis: 30%;
+    padding: 20px;
+    background-color: #f8f8f8;
+    border-radius: 5px;
+    box-shadow: 0 2px 4px rgba(0,0,0,0.1);
 }
 
-// Funciones para el manejo de arrastrar y soltar
-
-let draggedTask = null;
-
-function dragStartHandler(event) {
-    draggedTask = this;
-    event.dataTransfer.effectAllowed = 'move';
-    event.dataTransfer.setData('text/plain', event.target.textContent);
-    this.classList.add('dragged');
+.list h2 {
+    margin-top: 0;
+    font-size: 18px;
+    font-weight: bold;
+    color: #333;
 }
 
-function dragEndHandler(event) {
-    draggedTask = null;
-    this.classList.remove('dragged');
+.list-ul {
+    list-style-type: none;
+    padding: 0;
 }
 
-todoList.addEventListener('dragover', dragOverHandler);
-todoList.addEventListener('drop', dropHandler);
-doingList.addEventListener('dragover', dragOverHandler);
-doingList.addEventListener('drop', dropHandler);
-doneList.addEventListener('dragover', dragOverHandler);
-doneList.addEventListener('drop', dropHandler);
-
-function dragOverHandler(event) {
-    event.preventDefault();
-    event.dataTransfer.dropEffect = 'move';
+.list-ul li {
+    margin-bottom: 10px;
+    padding: 10px;
+    background-color: #fff;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    cursor: pointer;
 }
 
-function dropHandler(event) {
-    event.preventDefault();
-    const taskText = event.dataTransfer.getData('text/plain');
-    const taskElement = document.createElement('div');
-    taskElement.className = 'task';
-    taskElement.textContent = taskText;
-    taskElement.draggable = true;
-    taskElement.addEventListener('dragstart', dragStartHandler);
-    taskElement.addEventListener('dragend', dragEndHandler);
-    this.appendChild(taskElement);
-    if (draggedTask !== null) {
-        draggedTask.remove();
-    }
+.list-ul li:hover {
+    background-color: #efefef;
 }
